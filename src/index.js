aed637ee4943bb52f6dd78218da549550fc6a1ad
Akhilesh Kanojia <kanojiaakhilesh2010@gmail.com>
2019-01-06 23:35:03 +0530

    router
    react-router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,92 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Link, NavLink, Prompt, Switch } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Home = () => (
+    <div>
+        <h1>Home</h1>
+    </div>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const About = (props) => (
+    <div>
+        <h1>About</h1>
+        
+    </div>
+);
+
+const NotFound = (props) => (
+    <div>
+        <h1>NotFound</h1>
+        
+    </div>
+);
+
+class Form extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            isChanged: false
+        }
+    }
+    render(){
+        return(
+            <div>
+                <Prompt  when={this.state.isChanged} message={"Are you sure want to leave?"} />
+                <input type="text" 
+                   onChange={
+                       () => {
+                           this.setState({
+                            isChanged: true
+                           })
+                       }
+                   }
+                />
+                </div>
+        )
+    }
+}
+
+const Content = (props) => (
+    <div>
+        <NavLink className="list-group-item" exact activeClassName="active" to="/content/city">City</NavLink>
+        <NavLink className="list-group-item" activeClassName="active" to="/content/sports">Sports</NavLink>
+        <Route path="/content/:contentName" component={contentDetails} />
+    </div>
+);
+
+const contentDetails = (props) => (
+    <div>{props.match.params.contentName ? <div><img src={'http://lorempixel.com/400/200/'+props.match.params.contentName+'/1/'} /></div> : null}</div>
+)
+    
+
+const Links = () => (
+    <div class="list-group">
+        <NavLink className="list-group-item" exact activeClassName="active" to="/">Home</NavLink>
+        <NavLink className="list-group-item" activeClassName="active" to="/about">About</NavLink>
+        <NavLink className="list-group-item" activeClassName="active" to="/content">Content</NavLink>
+        <NavLink className="list-group-item" activeClassName="active" to="/form">Form</NavLink>
+    </div>
+)
+
+
+
+ReactDOM.render(
+    <Router>
+        <div className="row">
+           <section className="col-sm-4"> <Links /></section>
+           <section className="col-sm-8">
+           <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/content" component={Content} />
+            <Route path="/form" component={Form} />
+            <Route  component={NotFound} />
+           {/*<Route  render={() => <h1>NotFound page</h1>}/> use this if u want to 404 error msg large*/} 
+            </Switch>
+            </section>
+        </div>
+    </Router>
+    , document.getElementById('root'));
+
